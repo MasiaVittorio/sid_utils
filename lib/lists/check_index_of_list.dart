d5282d63bfab6467335137a4005cf9332fcbacf5
MasiaVittorio <14254644+MasiaVittorio@users.noreply.github.com>
2022-08-10 17:59:13 +0200

    fix

--- lib/lists/check_index_of_list.dart
+++ lib/lists/check_index_of_list.dart
@@ -6,11 +6,21 @@ bool checkIndexOfList(int index, List list) {
   return true;
 }
 
-extension ListSidIndexCheck on List {
-  bool checkIndex(int index) {
-    if(this.isEmpty) return false;
+extension ListSidIndexCheck<T> on List<T> {
+  bool checkIndex(int? index) {
+    if(index == null) return false;
+    if(isEmpty) return false;
     if(index < 0) return false;
-    if(index >= this.length) return false;
+    if(index >= length) return false;
     return true;
+  }
+
+  T? safeAt(int? index){
+    if(isEmpty) return null;
+    if(index == null) return null;
+    if(index < 0) return null;
+    if(index >= length) return null;
+    return elementAt(index);
+    
   }
 }
